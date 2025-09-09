5061bd8c972f3d836312562b77c25c99f7f11d8b
VocadoA <osinflesh@gmail.com>
2025-09-10 00:06:16 +0900

    XGB 모델링 코드 작성

--- 코드_공유.py
+++ 코드_공유.py
@@ -11,6 +11,8 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
+from xgboost import XGBClassifier
+
 wine = load_wine()
 
 # feature로 사용할 데이터에서는 'target' 컬럼을 drop합니다.
@@ -63,3 +65,33 @@ plt.show()
 
 ''' 코드 작성 바랍니다 '''
 
+# XGboost 모델 생성
+xgb_model = XGBClassifier(random_state=42)
+
+# 하이퍼파라미터 범위지정
+xgb_params = {
+    "max_depth" : [3, 5, 7, 9, 15],
+    "learning_rate" : [0.1, 0.01, 0.001],
+    "n_estimators": [50, 100, 200, 300]
+}
+
+# 하이퍼파라미터 최적화 및 학습
+grid_search = GridSearchCV(estimator=xgb_model, param_grid=xgb_params, cv=5, n_jobs=-1)
+grid_search.fit(X_train, y_train)
+
+# 테스트 데이터에 대한 예측
+best_xgb_model = grid_search.best_estimator_
+y_pred_xgb = best_xgb_model.predict(X_test)
+accuracy_xgb = accuracy_score(y_test, y_pred_xgb)
+print('Accuracy Grid :', accuracy_xgb)
+
+# Feature Importance 시각화
+importances_xgb = best_xgb_model.feature_importances_
+
+plt.figure(figsize = (20,6))
+plt.bar(range(len(importances_xgb)), importances_xgb, width=0.3)
+plt.xlabel('Feature')
+plt.ylabel('importances')
+plt.title('Feature Importance')
+plt.xticks(range(len(importances_xgb)), X.columns, rotation = 45)
+plt.show()
